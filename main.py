f07fb71cd978a5191af68a3f65b98c3613326fd0
Jean Paul SOSSAH <91275340+hawa029@users.noreply.github.com>
2023-05-03 18:13:04 +0200

    update main.py via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Import des bibliothèques nécessaires
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, flash
 from werkzeug.utils import secure_filename
 from keras.models import load_model, Model
 from keras.applications.vgg19 import VGG19
@@ -9,8 +9,7 @@ from keras.applications.vgg19 import preprocess_input
 from keras.utils import pad_sequences
 from numpy import argmax
 from joblib import load
-import pyttsx3
-import time
+
 
 
 import base64
@@ -32,31 +31,20 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # Définir la route pour la page d'accueil
 @app.route('/', methods=['GET','POST'])
 def home():
-    if request.method == 'POST':
-        # Vérifier si un fichier a été soumis
-        if 'file' not in request.files:
-            return redirect(request.url)
-        file = request.files['file']
-        # Vérifier si le fichier a un nom valide
-        if file.filename == '':
-            return redirect(request.url)
-        # Vérifier si l'extension du fichier est valide
-        if not allowed_file(file.filename):
-            return redirect(request.url)
-        # Enregistrer le fichier et rediriger vers la page d'annotation
     return render_template('home.html')
 
 # Définir la route pour la page de chargement de l'image
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
+
         # Vérifier si un fichier a été soumis
         if 'file' not in request.files:
             return redirect(request.url)
         file = request.files['file']
         # Vérifier si le fichier a un nom valide
         if file.filename == '':
-            return redirect(request.url)
+            flash("Veuillez insérer une image")
         # Vérifier si l'extension du fichier est valide
         if not allowed_file(file.filename):
             return redirect(request.url)
@@ -117,26 +105,13 @@ def view_annotation(filename):
     # code pour la traduction française
     from translate import Translator
  
-    # Create a translator object
+    # Creation d'un objet translator
     translator = Translator(from_lang="en", to_lang="fr")
  
-    # Translate a piece of text
+    # Traduction de la descritpion
     translation = translator.translate(description)
 
-    # code pour la synthèse vocale
-    eng = pyttsx3.init()
-    voice = eng.getProperty('voices') #get the available voices
-    rate = eng.getProperty('rate')
-    eng.setProperty('voice', voice[0].id) #set the voice to index 0 for male voice
-    # eng.setProperty('voice', voice[1].id) #changing voice to index 1 for female voice
-    eng.setProperty('rate', rate-50)
-    eng.say(translation)
-    eng.runAndWait()
-        
-
     return render_template('view_annotation.html', img_path=img_data, desc=translation)
-
-
 
 # Définir la route pour la page d'aide
 @app.route('/help')
